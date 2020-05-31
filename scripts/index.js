989067574ec91f11a887e7e8d627b3a2738cb4c2
BuildTools <Nickers@mail.ru>
2020-05-31 19:34:52 +0500

    index.js cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,5 @@
 
 $(document).ready(function () {
-    var $playfield = $('#playfield');
-    var mousedown = false;
-
     // Запускаем игру game.js
     new Game();
-
-    $playfield.unbind('mousedown').mousedown(function () {
-        mousedown = true;
-    });
-    $playfield.unbind('mouseup').mouseup(function () {
-        mousedown = false;
-    });
 });
